8a3c83d815b028d3c809e46681d1306b6f9b6dfc
atSeaer <jrd.seaer@gmail.com>
2023-03-17 07:19:29 -0700

    Initial commit

---
+++ venv/functions.py
@@ -0,0 +1,12 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ read a text file and return the list to-do item."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+def write_todos(todos_arg, filepath=FILEPATH):
+    """write the to do items list in the text file."""
+    with open(FILEPATH, 'w') as file:
+            file.writelines(todos_arg)
